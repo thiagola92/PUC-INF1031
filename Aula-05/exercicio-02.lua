9f2804b0606f0c9ba526b3d8d4aadcd5856ccf53
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2018-08-27 16:36:50 -0300

    Update exercicio-02.lua

--- Aula-05/exercicio-02.lua
+++ Aula-05/exercicio-02.lua
@@ -5,5 +5,22 @@ function palindrome(s)
     return true
   end
 
-  return 
+  local first_letter = string.sub(s, 1, 1)
+  local last_letter = string.sub(s, len, len)
+  
+  if(first_letter ~= last_letter) then
+    return false
+  end
+  
+  local new_string = string.sub(s, 2, len - 1)
+  return palindrome(new_string)
+end
+
+print("Diga a frase")
+local line = io.read("*line")
+
+if(palindrome(line)) then
+  print("É palíndrome")
+else
+  print("Não é palíndrome")
 end

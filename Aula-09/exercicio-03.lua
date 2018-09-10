1e41ed4ac352fd19f585a38d791af0ce16eaf1a3
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2018-09-10 16:48:24 -0300

    Add files via upload

--- Aula-09/exercicio-03.lua
+++ Aula-09/exercicio-03.lua
@@ -1,10 +1,4 @@
 function mdc(x, y)
-  if(x < y) then
-    local temp = x
-    x = y
-    y = temp
-  end
-  
   local r = x % y
   
   while(r ~= 0) do
@@ -22,5 +16,6 @@ function mdc(x, y)
 end
 
 print("Diga os dois numeros que voce quer saber o MDC:")
-
-print(mdc(1800, 2020))
+n1 = io.read("*n")
+n2 = io.read("*n")
+print(mdc(n1, n2))

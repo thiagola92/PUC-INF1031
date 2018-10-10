784a576fde143c8d5e576cb10baf336e8573a7f2
thiagola92 <thiagola92@gmail.com>
2018-10-09 22:32:48 -0300

    Aula-17

--- Aula-17/text.lua
+++ Aula-17/text.lua
@@ -9,40 +9,107 @@ local cores = {
   vermelho = {1, 0, 0},
   verde_claro = {0, 0.5, 0},
   azul_claro = {0, 0, 0.5},
-  random = {0, 0, 0},
 }
 
 function text.new(fontname, size, str)
   local meutexto = {}
-  
+
   local fonte = love.graphics.newFont(fontname .. ".ttf", size)
-  local texto = love.graphics.newText(fonte, str)
-  
+  --local texto = love.graphics.newText(fonte, str)
+
   meutexto.texto = texto
   meutexto.cor = cores.preto
   meutexto.ancora = "no"
-  
+
   return meutexto
 end
 
 function text.setString(obj, str)
-  
+  local fonte = obj.getFont()
+  --local texto = love.graphics.newText(fonte, str)
+
+  meutexto.texto = texto
+end
+
+local function setColorString(obj, str)
+  if(cores.str ~= nil) then
+    obj.cor = cores.str
+    return
+  end
+
+  if(cores.str == "random") then
+    local start = 1
+    local stop = math.random(6)
+
+    for k, v in pairs(cores) do
+      if(start == stop) then
+        obj.cor = v
+        break
+      end
+
+      start = start + 1
+    end
+
+    return
+  end
+
+  obj.cor = cores.preto
 end
 
 function text.setColor(obj, r, g, b)
   if(type(r) == "string") then
+    setColorString(obj, r)
+  else
+    obj.cor = {r, g, b}
   end
 end
 
 function text.setAnchor(obj, value)
+  obj.ancora = value
 end
 
 function text.getDimensions(obj)
-  
-  return 0, 0
+  local width, height = obj.texto.getDimensions()
+
+  return width, height
+end
+
+local function newPosition(x, y, width, height, ancora)
+  if(ancora == "c") then
+    x = x + width/2
+    y = y + height/2
+  elseif(ancora == "n") then
+    x = x + width/2
+  elseif(ancora == "s") then
+    x = x + width/2
+    y = y + height
+  elseif(ancora == "e") then -- leste deveria ser letra l
+    x = x + width
+    y = y + height
+  elseif(ancora == "o") then
+    y = y + height/2
+  elseif(ancora == "ne") then
+    x = x + width
+  elseif(ancora == "se") then
+    x = x + width
+    y = y + height
+  elseif(ancora == "so") then
+    y = y + height
+  end
+
+  return x, y
 end
 
 function text.draw(obj, x, y)
+  love.graphics.push()
+
+  local width, height = obj.texto.getDimensions()
+
+  x, y = newPosition(x, y, width, height, obj.ancora)
+  setColor(obj.cor)
+  love.graphics.draw(obj.texto, x, y)
+
+  love.graphics.pop()
 end
 
 return text

ebb16855f34cb7967e765d1e7e4e8a145da39421
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2018-11-06 22:13:38 -0200

    Add files via upload

--- Aula-23/love-acendeled/main.lua
+++ Aula-23/love-acendeled/main.lua
@@ -3,7 +3,7 @@ msgr = require "mqttLoveLibrary"
 local minhamat = '1721629' -- seu numero de matricula!
 local W, H = 400, 300
 
-local estadolove = false
+local mostrabaleia = false
 
 function love.keypressed(key)
   if(key == "return") then
@@ -11,20 +11,15 @@ function love.keypressed(key)
   end
 end
 
-local function mensagemRecebida (mensagem)
-  if estadolove == true then
-    love.graphics.setBackgroundColor (1 ,56/255 ,0)
-  else 
-    love.graphics.setBackgroundColor (56/255 ,1 ,0)
-  end
-  
-  estadolove = not estadolove
+local function mensagemRecebida (mensagem)  
+  mostrabaleia = not mostrabaleia
 end
 
 function love.load()
   msgr.start(minhamat, minhamat .. "love",  mensagemRecebida)  
   love.window.setMode(W,H)
   love.graphics.setBackgroundColor (1 ,56/255 ,0)
+  baleia = love.graphics.newImage("whale.png")
 end
 
 function love.update(dt)
@@ -33,5 +28,10 @@ end
 
 function love.draw()
   love.graphics.print("Aperte <enter>!", W/3, H/10)
+  
+  if mostrabaleia then
+    love.graphics.draw(baleia, W/3, H/3, 0, 0.5)
+  end
+  
 end
 
